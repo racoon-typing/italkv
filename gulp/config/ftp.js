ed7b3cecd77376e89a1be6d8dbf2212063b04d74
Daniil <eo18622@yandex.ru>
2022-11-04 10:46:54 +0300

    изм

--- gulp/config/ftp.js
+++ gulp/config/ftp.js
@@ -1,6 +1,6 @@
 export let configFTP = {
-    host: "194.58.102.130", // Адрес FTP сервера
-    user: "frontend", // Имя пользователя
-    password: "vzu_krw6EDZ*cdn3pwk", // Пароль
+    host: "185.114.247.197", // Адрес FTP сервера
+    user: "cg69657", // Имя пользователя
+    password: "QjVYZti2qlVO", // Пароль
     parallel: 5 // Кол-во одновременных потоков
 }
--- gulp/config/path.js
+++ gulp/config/path.js
@@ -31,5 +31,5 @@ export const path = {
     buildFolder: buildFolder,
     srcFolder: srcFolder,
     rootFolder: rootFolder,
-    ftp: 'offer'
+    ftp: 'publick_html'
 }
