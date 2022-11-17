6cbebe55ff5ffcad5211ffd021c378e786f5a5d4
Daniil <eo18622@yandex.ru>
2022-11-17 10:47:03 +0300

    пароль

--- gulp/config/ftp.js
+++ gulp/config/ftp.js
@@ -1,6 +1,6 @@
 export let configFTP = {
     host: "185.114.247.197", // Адрес FTP сервера
     user: "cg69657", // Имя пользователя
-    password: "QjVYZti2qlVO", // Пароль
+    password: "uwQSufy3", // Пароль
     parallel: 5 // Кол-во одновременных потоков
 }
